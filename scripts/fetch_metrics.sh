4f2ab9b0fed4d75b9d7df111ac5857df065d18a9
Harshit R <harshit127r@gmail.com>
2026-07-09 13:44:37 +0530

    Add memory and disk usage metrics fetching script

--- scripts/fetch_metrics.sh
+++ scripts/fetch_metrics.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+MEM=$(free -m | awk 'NR==2{printf "%.0f", $3*100/$2}')
+DISK=$(df -h / | awk 'NR==2{print $5}' | tr -d '%')
+echo "{\"memory\":$MEM,\"disk\":$DISK}" > /var/www/html/metrics.json
